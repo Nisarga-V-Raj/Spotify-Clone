cc0e82baa6ccac9dd42d94225d8702b9c345f34a
Nisarga_V_Raj <nisargavraj2003@gmail.com>
2025-03-07 02:26:43 +0530

    Updated Admin Changes

--- spotify-admin/src/pages/AddAlbum.jsx
+++ spotify-admin/src/pages/AddAlbum.jsx
@@ -7,9 +7,15 @@ const AddAlbum = () => {
   const [colour, setColour] = useState("#ffffff");
   const [name, setName] = useState("");
   const [desc, setDesc] = useState("");
-  const [loading, loading] = useState(false);
+  const [loading, setLoading] = useState(false);
 
-  return (
+  return loading ? (
+
+    <div className='flex flex-col gap-2.5'>
+      <p>Album Name</p>
+      <input className='bg-transparent outline-green-600 border-2 border-gray-400 p-2.5 w-[max(40vw, 250px)] type="text" placeholder=' Type Here' />
+    </div>
+  ) : (
     <form className='flex flex-col items-start gap-8 text-gray-600' >
       <div className='flex flex-col gap-4'>
         <p>Upload Image</p>
@@ -21,17 +27,17 @@ const AddAlbum = () => {
 
       <div className='flex flex-col gap-2.5'>
         <p>Album Name</p>
-        <input className='bg-transparent outline-green-600 border-2 border-gray-400 p-2.5 w-[max(40vw, 250px)]' type="text" placeholder=' Type Here' />
+        <input onChange={() => setName(e.target.value)} value={name} className='bg-transparent outline-green-600 border-2 border-gray-400 p-2.5 w-[max(40vw, 250px)]' type="text" placeholder=' Type Here' />
       </div>
 
       <div className='flex flex-col gap-2.5'>
         <p>Album Description</p>
-        <input className='bg-transparent outline-green-600 border-2 border-gray-400 p-2.5 w-[max(40vw, 250px)]' type="text" placeholder=' Type Here' />
+        <input onChange={() => setDesc(e.target.value)} value={desc} className='bg-transparent outline-green-600 border-2 border-gray-400 p-2.5 w-[max(40vw, 250px)]' type="text" placeholder=' Type Here' />
       </div>
 
       <div className='flex flex-col gap-3'>
         <p>Background Color</p>
-        <input type="color" />
+        <input onChange={(e) => setColour(e.target.value)} value={ } type="color" />
       </div>
 
       <button className='text-base bg-black text-white py-2.5 px-14 cursor-pointer' type='submit'>ADD</button>
