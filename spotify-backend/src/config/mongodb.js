68c97d3766b2d3af1ed45dfed06dc94ac6f20277
Nisarga_V_Raj <nisargavraj2003@gmail.com>
2025-03-06 20:16:59 +0530

    Updated backend features

--- spotify-backend/src/config/mongodb.js
+++ spotify-backend/src/config/mongodb.js
@@ -5,7 +5,7 @@ const connectDB = async () => {
         console.log("Connection Established");
         
     })
-    await mongoose.connect(`${process.env.MONGODB_URI}/spotify`);
+    await mongoose.connect(`${process.env.MONGODB_URI}/SpotifyClone`);
 }
 
 export default connectDB;
