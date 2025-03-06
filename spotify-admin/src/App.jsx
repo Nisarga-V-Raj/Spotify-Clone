83c23e3cb742c1fe84fdfb6018cfbcbac5550fc5
Nisarga_V_Raj <nisargavraj2003@gmail.com>
2025-03-07 00:22:08 +0530

    Updated Admin Changes

--- spotify-admin/src/App.jsx
+++ spotify-admin/src/App.jsx
@@ -7,13 +7,15 @@ import AddAlbum from './pages/AddAlbum';
 import ListSong from './pages/ListSong';
 import ListAlbum from './pages/ListAlbum';
 import Sidebar from './components/Sidebar';
+import Navbar from './components/Navbar';
 
 const App = () => {
   return (
     <div className='flex items-start min-h-screen'>
       <ToastContainer />
-      <Sidebar/>
+      <Sidebar />
       <div className='flex-1 h-screen overflow-y-scroll bg-[#F3FFF7]'>
+        <Navbar />
         <div className='pt-8 pl-5 sm:pt-12 sm:pl-12'>
           <Routes>
             <Route path='/add-song' element={<AddSong />} />
--- spotify-admin/src/pages/AddSong.jsx
+++ spotify-admin/src/pages/AddSong.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import { assets } from '../assets/assets'
 
 const AddSong = () => {
   return (
-    <div></div>
+    <form className='flex flex-col items-start gap-8 text-gray-600'>
+      <div className='flex gap-8'>
+        <div className='flex flex-col gap-4'>
+          <p>Upload Song</p>
+          <input type='file' id='song' accept='audio/*' hidden />
+          <label htmlFor="song">
+            <img src={assets.upload_song} className='w-24 cursor-pointer' alt="" />
+          </label>
+        </div>
+      </div>
+    </form>
   )
 }
 
