083c23a9cdf44a932f29575f1927384b7a55b0ee
Nisarga_V_Raj <nisargavraj2003@gmail.com>
2025-03-07 00:24:31 +0530

    Updated Admin Changes

--- spotify-admin/src/pages/AddSong.jsx
+++ spotify-admin/src/pages/AddSong.jsx
@@ -12,6 +12,14 @@ const AddSong = () => {
             <img src={assets.upload_song} className='w-24 cursor-pointer' alt="" />
           </label>
         </div>
+
+        <div className='flex flex-col gap-4'>
+          <p>Upload Image</p>
+          <input type='file' id='image' accept='image/*' hidden />
+          <label htmlFor="image">
+            <img src={assets.upload_area} className='w-24 cursor-pointer' alt="" />
+          </label>
+        </div>
       </div>
     </form>
   )
