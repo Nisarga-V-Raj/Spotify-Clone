e7ab21eb885a22e5ea583cd02f957191dbd21213
Nisarga_V_Raj <nisargavraj2003@gmail.com>
2025-03-06 20:23:35 +0530

    Updated backend features

--- spotify-backend/src/controllers/songController.js
+++ spotify-backend/src/controllers/songController.js
@@ -33,14 +33,19 @@ const addSong = async (req, res) => {
 const listSong = async (req, res) => {
     try {
         const allSongs = await songModel.find({});
-        res.json({success:true, songs: allSongs});
+        res.json({ success: true, songs: allSongs });
     } catch (error) {
-        res.json({success:false});
+        res.json({ success: false });
     }
 }
 
 const removeSong = async (req, res) => {
-
+    try {
+        await songModel.findByIdAndDelete(req.body.id);
+        res.json({ success: true, message: "Song Removed" })
+    } catch (error) {
+        res.json({ success: false });
+    }
 }
 
 export { addSong, listSong, removeSong }
