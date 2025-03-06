99a6afd81a15eaa288e37eb2cd3f6fb346aa840b
Nisarga_V_Raj <nisargavraj2003@gmail.com>
2025-03-06 20:06:38 +0530

    Updated backend features

--- spotify-backend/src/controllers/songController.js
+++ spotify-backend/src/controllers/songController.js
@@ -1,4 +1,4 @@
-import {v2 as cloudinary} from 'cloudinary'
+import { v2 as cloudinary } from 'cloudinary'
 import songModel from '../models/songModel.js';
 
 const addSong = async (req, res) => {
@@ -8,12 +8,19 @@ const addSong = async (req, res) => {
         const album = req.body.album;
         const audioFile = req.files.audio[0];
         const imageFile = req.files.image[0];
-        const audioUpload = await cloudinary.uploader.upload(audioFile.path, {resource_type: "video"});
-        const imageUpload = await cloudinary.uploader.upload(imageFile.path, {resource_type: "image"});
-        console.log(name, desc, album, audioUpload, imageUpload);
-
+        const audioUpload = await cloudinary.uploader.upload(audioFile.path, { resource_type: "video" });
+        const imageUpload = await cloudinary.uploader.upload(imageFile.path, { resource_type: "image" });
+        const duration = `${Math.floor(audioUpload.duration / 60)}:${Math.floor(audioUpload.duration % 60)}`
+        const songData = {
+            name,
+            desc,
+            album,
+            image: imageUpload.secure_url,
+            file: audioUpload.secure_url,
+            duration
+        }
     } catch (error) {
-        
+
     }
 }
 
@@ -21,4 +28,4 @@ const listSong = async (req, res) => {
 
 }
 
-export {addSong, listSong}
+export { addSong, listSong }
