4a0c18baeab80f2421f2c0dba054ff52eaa94330
Nisarga_V_Raj <nisargavraj2003@gmail.com>
2025-03-07 00:11:25 +0530

    Updated Admin Changes

--- spotify-admin/src/components/Sidebar.jsx
+++ spotify-admin/src/components/Sidebar.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { assets } from '../assets/assets'
+import { NavLink } from 'react-router-dom'
 const Sidebar = () => {
     return (
         <div className='bg-[#003A10] min-h-screen pl-[4vw]'>
@@ -7,10 +8,10 @@ const Sidebar = () => {
             <img src={assets.logo_small} className='mt-5 w-[max(5vw, 40px)] mr-5 sm:hidden block' alt="" />
 
             <div className='flex flex-col gap-5 mt-10'>
-                <div className='flex items-centergap-2.5 text-gray-800 bg-white border border-black p-2 pr-[max(8vw, 10px)] drop-shadow-[4px_4px_#00FF5B] text-sm font-medium'>
+                <NavLink to='/add-song' className='flex items-centergap-2.5 text-gray-800 bg-white border border-black p-2 pr-[max(8vw, 10px)] drop-shadow-[4px_4px_#00FF5B] text-sm font-medium'>
                     <img src={assets.add_song} className='w-5 ' alt="" />
                     <p className='hidden sm:block'>Add Song</p>
-                </div>
+                </NavLink>
 
                 <div className='flex items-centergap-2.5 text-gray-800 bg-white border border-black p-2 pr-[max(8vw, 10px)] drop-shadow-[4px_4px_#00FF5B] text-sm font-medium'>
                     <img src={assets.song_icon} className='w-5 ' alt="" />
