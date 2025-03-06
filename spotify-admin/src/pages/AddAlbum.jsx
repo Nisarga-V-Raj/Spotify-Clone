0339d5a51043ab682c61c3976ea21d7b96059cd0
Nisarga_V_Raj <nisargavraj2003@gmail.com>
2025-03-07 01:57:40 +0530

    Updated Admin Changes

--- spotify-admin/src/pages/AddAlbum.jsx
+++ spotify-admin/src/pages/AddAlbum.jsx
@@ -6,15 +6,22 @@ const AddAlbum = () => {
     <form className='flex flex-col items-start gap-8 text-gray-600' >
       <div className='flex flex-col gap-4'>
         <p>Upload Image</p>
-        <input type='file' id='image' accept='image/*' hidden/>
+        <input type='file' id='image' accept='image/*' hidden />
         <label htmlFor="image">
           <img className='w-24 cursor-pointer' src={assets.upload_area} alt="" />
         </label>
       </div>
+
       <div className='flex flex-col gap-2.5'>
         <p>Album Name</p>
-        <input className='bg-transparent outline-green-600 border-2 border-gray-400 p-2.5 w-[max(40vw, 250px)] type="text" placeholder='Type Here' />
+        <input className='bg-transparent outline-green-600 border-2 border-gray-400 p-2.5 w-[max(40vw, 250px)]' type="text" placeholder=' Type Here' />
       </div>
+
+      <div className='flex flex-col gap-2.5'>
+        <p>Album Description</p>
+        <input className='bg-transparent outline-green-600 border-2 border-gray-400 p-2.5 w-[max(40vw, 250px)]' type="text" placeholder=' Type Here' />
+      </div>
+
     </form>
   )
 }
