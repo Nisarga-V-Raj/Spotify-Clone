cfbd9bbe35ae58f2604e4cf505d3d08abdca7540
Nisarga_V_Raj <nisargavraj2003@gmail.com>
2025-03-07 01:51:11 +0530

    Updated Admin Changes

--- spotify-admin/src/pages/AddAlbum.jsx
+++ spotify-admin/src/pages/AddAlbum.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import { assets } from '../assets/assets'
 
 const AddAlbum = () => {
   return (
-    <div></div>
+    <form className='flex flex-col items-start gap-8 text-gray-600' >
+      <div className='flex flex-col gap-4'>
+        <p>Upload Image</p>
+        <input type='file' id='image' accept='image/*' hidden/>
+        <label htmlFor="image">
+          <img className='w-24 cursor-pointer' src={assets.upload_area} alt="" />
+        </label>
+      </div>
+    </form>
   )
 }
 
