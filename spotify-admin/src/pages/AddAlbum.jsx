53479d633117362235ab8ab870c388af5b2dca83
Nisarga_V_Raj <nisargavraj2003@gmail.com>
2025-03-07 15:37:07 +0530

    Updated Admin Changes

--- spotify-admin/src/pages/AddAlbum.jsx
+++ spotify-admin/src/pages/AddAlbum.jsx
@@ -36,8 +36,9 @@ const AddAlbum = () => {
       console.log("Failed to add album", error);
       toast.error("Failed to add album");
     }
+    setLoading(false);
   }
-  
+
   return loading ? (
     <div className='grid place-items-center min-h-[80vh]'>
       <div className='w-16 h-16 place-self-center border-4 border-gray-400 border-t-green-800 rounded-full animate-spin'></div>
