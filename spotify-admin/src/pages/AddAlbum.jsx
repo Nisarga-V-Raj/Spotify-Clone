60e059bedbb2c1bb46b4ad585f220b4c3ce0a3ea
Nisarga_V_Raj <nisargavraj2003@gmail.com>
2025-03-07 15:11:18 +0530

    Updated Admin Changes

--- spotify-admin/src/pages/AddAlbum.jsx
+++ spotify-admin/src/pages/AddAlbum.jsx
@@ -1,8 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { assets } from '../assets/assets'
 
 const AddAlbum = () => {
+
+  const [image, setImage] = useState(false);
+  const [colour, setColour] = useState("#121212");
+  const [name, setName] = useState("");
+  const [desc, setDesc] = useState("");
+  const [loading, setLoading] =useState(false);
+
   return (
-    <div>AddAlbum</div>
+    <form className='flex flex-col items-start gap-8 text-gray-600'>
+      <div className='flex flex-col gap-4'>
+        <p>Upload Image</p>
+        <input onChange={(e)=>setImage(e.target.files[0])} type="file" id="image" accept='image' hidden/>
+        <label htmlFor="image">
+          <img className='w-24 cursor-pointer' src={assets.upload_area} alt="" />
+        </label>
+      </div>
+
+      <div className='flex flex-col gap-2.5'>
+        <p>Album Name</p>
+        <input className='bg-transparent outline-green-600 border-2 border-gray-400 p-2.5 w-[max(40vw, 250px)]' type="text" placeholder='Type Here' />
+      </div>
+
+      <div className='flex flex-col gap-2.5'>
+        <p>Album Description</p>
+        <input className='bg-transparent outline-green-600 border-2 border-gray-400 p-2.5 w-[max(40vw, 250px)]' type="text" placeholder='Type Here' />
+      </div>
+
+      <div className='flex flex-col gap-3'>
+        <p>Background Color</p>
+        <input type="color" />
+      </div>
+
+      <button className='text-base bg-black text-white py-2.5 px-14 cursor-pointer' type="submit">ADD</button>
+
+    </form>
   )
 }
 
