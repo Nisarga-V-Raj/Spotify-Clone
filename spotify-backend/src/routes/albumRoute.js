c84f5f70e6c4ab01c09fb206e3838a3b517d3a23
Nisarga_V_Raj <nisargavraj2003@gmail.com>
2025-03-07 19:55:25 +0530

    Updated Backend features

--- spotify-backend/src/routes/albumRoute.js
+++ spotify-backend/src/routes/albumRoute.js
@@ -1,11 +1,11 @@
-import express from 'express'
-import { addAlbum, listAlbum, removeAlbum } from '../controllers/albumController.js'
-import upload from '../middleware/multer.js'
+import express from 'express';
+import { addAlbum, listAlbum, removeAlbum } from '../controllers/albumController.js';
+import upload from '../middleware/multer.js';
 
 const albumRouter = express.Router();
 
 albumRouter.post('/add', upload.single('image'), addAlbum);
 albumRouter.get('/list', listAlbum);
-albumRouter.post('/remove', removeAlbum);
+albumRouter.delete('/remove', removeAlbum); // Changed from POST to DELETE
 
 export default albumRouter;
