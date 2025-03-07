87c837a4912168571a3a5357ff65e933751e26d1
Nisarga_V_Raj <nisargavraj2003@gmail.com>
2025-03-07 16:09:00 +0530

    Updated Admin Changes

--- spotify-clone/src/context/PlayerContext.jsx
+++ spotify-clone/src/context/PlayerContext.jsx
@@ -1,5 +1,5 @@
 import { createContext, useEffect, useRef, useState } from "react";
-import { songsData } from "../assets/assets";
+import axios from 'axios'
 
 export const PlayerContext = createContext();
 
@@ -9,6 +9,10 @@ const PlayerContextProvider = (props) => {
     const seekBg = useRef();
     const seekBar = useRef();
 
+    const url = 'http://localhost:4000';
+
+    const [songsData, setSongsData] =useState([]);
+    const [albumsData, setAlbumsData] = useState([]);
     const [track, setTrack] = useState(songsData[0]);
     const [playStatus, setPlayStatus] = useState(false);
     const [time, setTime] = useState({
@@ -58,6 +62,26 @@ const PlayerContextProvider = (props) => {
         audioRef.current.currentTime =((e.nativeEvent.offsetX / seekBg.current.offsetWidth) * audioRef.current.duration);
     }
 
+    const getSongsData = async () => {
+        try {
+            const response = await axios.get(`${url}/api/song/list`);
+            setSongsData(response.data.songs);
+            setTrack(response.data.songs[0]);
+
+        } catch (error) {
+            
+        }
+    }
+
+    const getAlbumsData =async () => {
+        try {
+            const response = await axios.get(`${url}/api/album/list`);
+            setAlbumsData(response.data.albums);
+        } catch (error) {
+            
+        }
+    }
+
     useEffect(() => {
         setTimeout(() => {
             audioRef.current.ontimeupdate = () => {
@@ -76,6 +100,11 @@ const PlayerContextProvider = (props) => {
         }, 1000);
     }, [audioRef])
 
+    useEffect(()=>{
+        getSongsData();
+        getAlbumsData();
+    },[])
+
     const contextValue = {
         audioRef,
         seekBg,
@@ -88,7 +117,9 @@ const PlayerContextProvider = (props) => {
         playWithId,
         previous,
         next,
-        seekSong
+        seekSong,
+        songsData,
+        albumsData
     }
 
     return (
