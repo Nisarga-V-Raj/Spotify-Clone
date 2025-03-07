7520f132f878116e8de710712c97fbc7eafa3c7c
Nisarga_V_Raj <nisargavraj2003@gmail.com>
2025-03-07 16:15:07 +0530

    Updated Admin Changes

--- spotify-clone/src/components/DisplayHome.jsx
+++ spotify-clone/src/components/DisplayHome.jsx
@@ -1,8 +1,6 @@
 import React from 'react'
 import Navbar from './Navbar'
-import { albumsData } from '../assets/assets'
 import AlbumItem from './AlbumItem'
-import { songsData } from '../assets/assets'
 import SongItem from './SongItem'
 import { useContext } from 'react'
 import { PlayerContext } from '../context/PlayerContext'
@@ -17,13 +15,13 @@ const DisplayHome = () => {
       <div className='mb-4'>
         <h1 className='my-5 font-bold text-2xl'>Featured Charts</h1>
         <div className='flex overflow-auto'>
-          {albumsData.map((item, index) => (<AlbumItem key={index} name={item.name} desc={item.desc} id={item.id} image={item.image} />))}
+          {albumsData.map((item, index) => (<AlbumItem key={index} name={item.name} desc={item.desc} id={item._id} image={item.image} />))}
         </div>
       </div>
       <div className='mb-4'>
         <h1 className='my-5 font-bold text-2xl'>Today&apos;s Biggest Hits</h1>
         <div className='flex overflow-auto'>
-          {songsData.map((item, index) => (<SongItem key={index} name={item.name} desc={item.desc} id={item.id} image={item.image} />))}
+          {songsData.map((item, index) => (<SongItem key={index} name={item.name} desc={item.desc} id={item._id} image={item.image} />))}
         </div>
       </div>
     </>
