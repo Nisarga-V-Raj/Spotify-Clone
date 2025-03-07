92560a57f3374f3a8e4dde7f69020a41780085cc
Nisarga_V_Raj <nisargavraj2003@gmail.com>
2025-03-07 16:55:33 +0530

    Updated Admin Changes

--- spotify-clone/src/context/PlayerContext.jsx
+++ spotify-clone/src/context/PlayerContext.jsx
@@ -11,7 +11,7 @@ const PlayerContextProvider = (props) => {
 
     const url = 'http://localhost:4000';
 
-    const [songsData, setSongsData] =useState([]);
+    const [songsData, setSongsData] = useState([]);
     const [albumsData, setAlbumsData] = useState([]);
     const [track, setTrack] = useState(songsData[0]);
     const [playStatus, setPlayStatus] = useState(false);
@@ -37,29 +37,37 @@ const PlayerContextProvider = (props) => {
     }
 
     const playWithId = async (id) => {
-        await setTrack(songsData[id]);
+        await songsData.map((item) => {
+            if (id === item._id) {
+                setTrack(item);
+            }
+        })
         await audioRef.current.play();
         setPlayStatus(true);
     }
 
     const previous = async () => {
-        if (track.id > 0) {
-            await setTrack(songsData[track.id - 1]);
-            await audioRef.current.play();
-            setPlayStatus(true);
-        }
+        songsData.map(async (item, index) => {
+            if (track._id === item._id && index > 0) {
+                await setTrack(songsData[index - 1]);
+                await audioRef.current.play();
+                setPlayStatus(true);
+            }
+        })
     }
 
     const next = async () => {
-        if (track.id < songsData.length - 1) {
-            await setTrack(songsData[track.id + 1]);
-            await audioRef.current.play();
-            setPlayStatus(true);
-        }
+        songsData.map(async (item, index) => {
+            if (track._id === item._id && index < songsData.length) {
+                await setTrack(songsData[index + 1]);
+                await audioRef.current.play();
+                setPlayStatus(true);
+            }
+        })
     }
 
     const seekSong = async (e) => {
-        audioRef.current.currentTime =((e.nativeEvent.offsetX / seekBg.current.offsetWidth) * audioRef.current.duration);
+        audioRef.current.currentTime = ((e.nativeEvent.offsetX / seekBg.current.offsetWidth) * audioRef.current.duration);
     }
 
     const getSongsData = async () => {
@@ -69,16 +77,17 @@ const PlayerContextProvider = (props) => {
             setTrack(response.data.songs[0]);
 
         } catch (error) {
-            
+            console.log("Failed to fetch songs", error);
+
         }
     }
 
-    const getAlbumsData =async () => {
+    const getAlbumsData = async () => {
         try {
             const response = await axios.get(`${url}/api/album/list`);
             setAlbumsData(response.data.albums);
         } catch (error) {
-            
+            console.log("Failed to fetch albums", error);
         }
     }
 
@@ -100,10 +109,10 @@ const PlayerContextProvider = (props) => {
         }, 1000);
     }, [audioRef])
 
-    useEffect(()=>{
+    useEffect(() => {
         getSongsData();
         getAlbumsData();
-    },[])
+    }, [])
 
     const contextValue = {
         audioRef,
