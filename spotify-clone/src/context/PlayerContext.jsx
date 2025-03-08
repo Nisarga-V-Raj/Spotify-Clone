2fb820cd9813e551b4fac0bac6933a57ba7d2b77
Nisarga_V_Raj <nisargavraj2003@gmail.com>
2025-03-08 20:04:28 +0530

    Updated Clone Changes

--- spotify-clone/src/context/PlayerContext.jsx
+++ spotify-clone/src/context/PlayerContext.jsx
@@ -1,5 +1,5 @@
 import { createContext, useEffect, useRef, useState } from "react";
-import { songsData } from "../assets/assets";
+import axios from 'axios';
 
 export const PlayerContext = createContext();
 
@@ -9,6 +9,10 @@ const PlayerContextProvider = (props) => {
     const seekBg = useRef();
     const seekBar = useRef();
 
+    const url = 'http:/localhost:4000';
+
+    const [songsData, setSongsData] = useState([]);
+    const [albumsData, setAlbumsData] = useState([]);
     const [track, setTrack] = useState(songsData[0]);
     const [playStatus, setPlayStatus] = useState(false);
     const [time, setTime] = useState({
@@ -56,6 +60,25 @@ const PlayerContextProvider = (props) => {
 
     const seekSong = async (e) => {
         audioRef.current.currentTime =((e.nativeEvent.offsetX / seekBg.current.offsetWidth) * audioRef.current.duration);
+    }
+
+    const getSongsData = async () => {
+        try {
+            const response = await axios.get(`${url}/api/song/list`);
+            setSongsData(response.data.songs);
+            setTrack(response.data.songs[0]);
+        } catch (error) {
+            console.log("Failed to fetch songs", error);
+            
+        }
+    }
+
+    const getAlbumsData = async () => {
+        try {
+            const response = await axios.get(`${url}/api/album/list`);
+        } catch (error) {
+            
+        }
     }
 
     useEffect(() => {
