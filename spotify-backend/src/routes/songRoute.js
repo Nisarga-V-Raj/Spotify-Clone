d5492bc9daccb6fb66e1e5f5370685db7ec98013
Nisarga_V_Raj <nisargavraj2003@gmail.com>
2025-03-09 18:57:43 +0530

    Updated backend features

--- spotify-backend/src/routes/songRoute.js
+++ spotify-backend/src/routes/songRoute.js
@@ -6,6 +6,6 @@ const songRouter = express.Router();
 
 songRouter.post('/add', upload.fields([{ name: 'image', maxCount: 1 }, { name: 'audio', maxCount: 1 }]), addSong);
 songRouter.get('/list', listSong);
-songRouter.post('/delete', removeSong); // Changed from POST to DELETE
+songRouter.post('/delete', removeSong);
 
 export default songRouter;
