e995e94d8577a94d46fb1ef5b0a31768afa3cd4c
Nisarga_V_Raj <nisargavraj2003@gmail.com>
2025-03-09 21:23:39 +0530

    Wrong commit restoring in process

--- spotify-clone/src/App.jsx
+++ spotify-clone/src/App.jsx
@@ -6,24 +6,18 @@ import { PlayerContext } from './context/PlayerContext'
 
 const App = () => {
 
-  const { audioRef, track, songsData, albumsData } = useContext(PlayerContext);
+  const { audioRef, track } = useContext(PlayerContext);
 
   return (
     <div className='h-screen bg-black text-white'>
-      {
-        songsData.length !== 0
-          ? <>
-            <div className='h-[90%] flex '>
-              <Sidebar />
-              <Display />
-            </div>
-            <Player />
-          </>
-          : null
-      }
-      <audio ref={audioRef} src={track ? track.file : null} preload='auto'></audio>
+      <div className='h-[90%] flex '>
+        <Sidebar />
+        <Display />
+      </div>
+      <Player />
+      <audio ref={audioRef} src={track.file} preload='auto'></audio>
     </div>
   )
 }
 
-export default App
+export default App;
