f5c02628943f3e56f1f77c13ed7481789564daeb
Nisarga_V_Raj <nisargavraj2003@gmail.com>
2025-03-19 17:42:50 +0530

    Updated Clone after retrieving wrong commit

--- spotify-clone/src/components/DisplayHome.jsx
+++ spotify-clone/src/components/DisplayHome.jsx
@@ -7,14 +7,14 @@ import { PlayerContext } from '../context/PlayerContext'
 
 const DisplayHome = () => {
 
-  const {songsData, albumsData} = useContext(PlayerContext);
+  const { songsData, albumsData } = useContext(PlayerContext);
 
   return (
     <>
       <Navbar />
       <div className='mb-4'>
         <h1 className='my-5 font-bold text-2xl'>Featured Charts</h1>
-        <div className='flex overflow-auto'>
+        <div className='grid grid-cols-5 gap-6'>
           {albumsData.map((item, index) => (<AlbumItem key={index} name={item.name} desc={item.desc} id={item._id} image={item.image} />))}
         </div>
       </div>
