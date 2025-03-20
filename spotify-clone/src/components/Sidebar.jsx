989868285e3e4eb7396bfc4fd8e9abc09969961f
Nisarga_V_Raj <nisargavraj2003@gmail.com>
2025-03-20 13:20:17 +0530

    Updated Clone after retrieving wrong commit

--- spotify-clone/src/components/Sidebar.jsx
+++ spotify-clone/src/components/Sidebar.jsx
@@ -3,12 +3,11 @@ import { assets } from '../assets/assets'
 import { useNavigate } from 'react-router-dom'
 
 const Sidebar = () => {
-
     const navigate = useNavigate();
 
     return (
         <div className='w-[25%] h-full p-2 flex-col gap-2 text-white hidden lg:flex'>
-            <div className='bg-[#121212] h-[15%] rounded flex flex-col justify-around'>
+            <div className='bg-[#121212] h-[20%] rounded flex flex-col justify-around'>
                 <div onClick={() => navigate('/')} className='flex items-center gap-3 pl-8 cursor-pointer'>
                     <img className='w-6' src={assets.home_icon} alt="" />
                     <p className='font-bold'>Home</p>
@@ -18,7 +17,8 @@ const Sidebar = () => {
                     <p className='font-bold'>Search</p>
                 </div>
             </div>
-            <div className='bg-[#121212] h-[85%] rounded'>
+
+            <div className='bg-[#121212] h-[85%] rounded flex flex-col overflow-y-auto max-h-[85%] scrollbar-thin scrollbar-thumb-gray-700 scrollbar-track-gray-900 pr-2'>
                 <div className='p-4 flex items-center justify-between'>
                     <div className='flex items-center gap-3'>
                         <img className='w-8' src={assets.stack_icon} alt="" />
@@ -29,16 +29,40 @@ const Sidebar = () => {
                         <img className='w-5' src={assets.plus_icon} alt="" />
                     </div>
                 </div>
+
                 <div className='p-4 bg-[#242424] m-2 rounded font-semibold flex flex-col items-start justify-start gap-1 pl-4'>
                     <h1>Create your first playlist</h1>
-                    <p className='font-light'>It&apos;s easy, we&apos;ll help you</p>
-                    <button className='px-4 py-1.5 bg-white text-[15px] text-black rounded-full mt-4'>Create Playlist</button>
+                    <p>It&apos;s easy, we&apos;ll help you</p>
+                    <button className='px-4 py-1.5 bg-white text-[15px] text-black rounded-full mt-4'>
+                        Create Playlist
+                    </button>
                 </div>
+
                 <div className='p-4 bg-[#242424] m-2 rounded font-semibold flex flex-col items-start justify-start gap-1 pl-4 mt-4'>
                     <h1>Let&apos;s find some podcasts to follow</h1>
                     <p>We&apos;ll keep you updated on new episodes</p>
-                    <button className='px-4 py-1.5 bg-white text-[15px] text-black rounded-full mt-4'>Browse Podcasts</button>
+                    <button className='px-4 py-1.5 bg-white text-[15px] text-black rounded-full mt-4'>
+                        Browse Podcasts
+                    </button>
                 </div>
+            </div>
+
+            <div className='mt-2 p-4 text-gray-400 text-sm'>
+                <ul className="space-y-2">
+                    <li className="flex gap-3">
+                        <a href="#" className="cursor-pointer">Legal</a>
+                        <a href="#" className="cursor-pointer">Safety & Privacy Center</a>
+                        <a href="#" className="cursor-pointer">Privacy Policy</a>
+                    </li>
+                    <li className="flex gap-3">
+                        <a href="#" className="cursor-pointer">Cookies</a>
+                        <a href="#" className="cursor-pointer">About Ads</a>
+                        <a href="#" className="cursor-pointer">Accessibility</a>
+                    </li>
+                    <li>
+                        <a href="#" className="text-white cursor-pointer hover:underline">Cookies</a>
+                    </li>
+                </ul>
             </div>
         </div>
     )
