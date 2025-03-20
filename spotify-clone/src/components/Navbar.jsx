25bb50258dd08c9000eb07789ad8eb2df2afdcc8
Nisarga_V_Raj <nisargavraj2003@gmail.com>
2025-03-20 14:19:13 +0530

    Final adjustments

--- spotify-clone/src/components/Navbar.jsx
+++ spotify-clone/src/components/Navbar.jsx
@@ -16,13 +16,14 @@ const Navbar = () => {
                 <div className='flex items-center gap-4'>
                     <p className='bg-white text-black text-[15px] px-4 py-1 rounded-2xl hidden md:block cursor-pointer transition-transform duration-200 hover:bg-gray-200 hover:scale-105'>Explore Premium</p>
                     <p className='bg-black py-1 px-3 rounded-2xl text-[15px] cursor-pointer transition-transform duration-200 hover:bg-gray-800 hover:scale-105'>Install App</p>
-                    <p className='bg-purple-500 text-black w-7 h-7 rounded-full flex items-center justify-center transition-transform duration-200 hover:bg-purple-400 hover:scale-105'>NV</p>
+                    <p className='bg-purple-500 text-black w-7 h-7 rounded-full cursor-pointer flex items-center justify-center transition-transform duration-200 hover:bg-purple-400 hover:scale-105'>NV</p>
                 </div>
             </div>
+
             <div className='flex items-center gap-2 mt-4'>
-                <p className='bg-white text-black px-4 py-1 rounded-2xl cursor-pointer'>All</p>
-                <p className='bg-black px-4 py-1 rounded-2xl cursor-pointer'>Music</p>
-                <p className='bg-black px-4 py-1 rounded-2xl cursor-pointer'>Podcasts</p>
+                <p className='bg-white text-black px-4 py-1 rounded-2xl cursor-pointer transition-transform duration-200 hover:bg-gray-200 hover:scale-105'>All</p>
+                <p className='bg-black px-4 py-1 rounded-2xl cursor-pointer transition-transform duration-200 hover:bg-gray-800 hover:scale-105'>Music</p>
+                <p className='bg-black px-4 py-1 rounded-2xl cursor-pointer transition-transform duration-200 hover:bg-gray-800 hover:scale-105'>Podcasts</p>
             </div>
         </>
     )
