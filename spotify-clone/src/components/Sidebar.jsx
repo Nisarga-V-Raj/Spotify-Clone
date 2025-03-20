d71e2d82ea95a41dd25aa7810622f7436a872825
Nisarga_V_Raj <nisargavraj2003@gmail.com>
2025-03-20 14:49:13 +0530

    Final adjustments

--- spotify-clone/src/components/Sidebar.jsx
+++ spotify-clone/src/components/Sidebar.jsx
@@ -32,17 +32,19 @@ const Sidebar = () => {
                 <div className='p-4 bg-[#242424] m-2 rounded font-semibold flex flex-col items-start justify-start gap-1 pl-4'>
                     <h1>Create your first playlist</h1>
                     <p>It&apos;s easy, we&apos;ll help you</p>
-                    <button className='px-4 py-1.5 bg-white text-[15px] text-black rounded-full mt-4'>
+                    <button className='px-4 py-1.5 bg-white text-[15px] text-black rounded-full mt-4 transition-transform duration-200 hover:scale-105'>
                         Create Playlist
                     </button>
+
                 </div>
 
                 <div className='p-4 bg-[#242424] m-2 rounded font-semibold flex flex-col items-start justify-start gap-1 pl-4 mt-4'>
                     <h1>Let&apos;s find some podcasts to follow</h1>
                     <p>We&apos;ll keep you updated on new episodes</p>
-                    <button className='px-4 py-1.5 bg-white text-[15px] text-black rounded-full mt-4'>
+                    <button className='px-4 py-1.5 bg-white text-[15px] text-black rounded-full mt-4 transition-transform duration-200 hover:scale-105'>
                         Browse Podcasts
                     </button>
+
                 </div>
             </div>
 
