a3c752648db2686138badab618abadb46db72488
Nisarga_V_Raj <nisargavraj2003@gmail.com>
2025-03-22 23:51:35 +0530

    Final adjustments

--- spotify-backend/src/routes/albumRoute.js
+++ spotify-backend/src/routes/albumRoute.js
@@ -6,6 +6,6 @@ const albumRouter = express.Router();
 
 albumRouter.post('/add', upload.single('image'), addAlbum);
 albumRouter.get('/list', listAlbum);
-albumRouter.post('/delete', removeAlbum); // Changed from POST to DELETE
+albumRouter.post('/delete', removeAlbum);
 
 export default albumRouter;
